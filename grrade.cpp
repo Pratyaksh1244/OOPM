ae799c7e8b6637c4706249ab6216bdee93c99536
Pratyaksh1244 <pratyaksh.patelcse2024@indoreinstitute.com>
2025-09-02 11:04:40 +0530

    Add files via upload

---
+++ grrade.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char grade;
+	cout<<"Enter your grade : ";
+	cin>>grade;
+	
+	switch(grade){
+		case 'A': cout<<"Outstanding";
+		break;
+		
+		case 'B': cout<<"Very good";
+		break;
+		
+		case 'C': cout<<"Average";
+		break;
+		
+		case 'D': cout<<"Poor";
+		break;
+		
+		case 'E': cout<<"Fail";
+		break;
+		
+		case 'a': cout<<"Outstanding";
+		break;
+		
+		case 'b': cout<<"Very good";
+		break;
+		
+		case 'c': cout<<"Average";
+		break;
+		
+		case 'd': cout<<"Poor";
+		break;
+		
+		case 'e': cout<<"Fail";
+		break;
+		
+	}
+}
